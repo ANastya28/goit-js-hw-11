c7f1a4f047aade08fe0daab375892d65c4c4e46a
ANastya28 <guselnikova2803@gmail.com>
2023-07-14 21:22:41 +0300

    created render pages

--- src/index.js
+++ src/index.js
@@ -1,3 +1,132 @@
+import Notiflix from 'notiflix';
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import axios from "axios";
 
 
+// const BASE_URL = 'https://pixabay.com/api/'
+const API_KEY = '38260386-cb0ba1fc5da3483268325876c';
 
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+
+const formEl = document.querySelector('.search-form');
+const galleryEl = document.querySelector('.gallery');
+const buttonLoadMore = document.querySelector('.load-more');
+
+formEl.addEventListener('submit', onSubmitForm);
+buttonLoadMore.addEventListener('click', onBtnLoadMoreClick);
+
+buttonLoadMore.classList.replace('load-more', 'is-hidden');
+
+function onSubmitForm(event) {
+  event.preventDefault();
+  const query = event.currentTarget.elements.searchQuery.value;
+  if (!query.trim() || query === queryToFetch) {
+    return;
+  }
+  queryToFetch = query;
+  galleryEl.innerHTML = '';
+  pageToFetch = 1;
+  getImages(queryToFetch, pageToFetch);
+  formEl.reset();
+}
+
+function onBtnLoadMoreClick() {
+  pageToFetch += 1;
+  getImages(queryToFetch, pageToFetch);
+}
+
+
+let queryToFetch = '';
+let pageToFetch;
+const pageLimit = 20;
+
+const fetchImages = async (queryToFetch, pageToFetch) => {
+  const { data } = await axios({
+    params: {
+      key: API_KEY,
+      q: queryToFetch,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      per_page: pageLimit,
+      page: pageToFetch,
+    },
+  });
+  return data;
+};
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+const renderImages = data => {
+  const markup = data.hits
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<a class="photo-link" href="${largeImageURL}"><div class="photo-card">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>${likes}
+    </p>
+    <p class="info-item">
+      <b>Views</b>${views}
+    </p>
+    <p class="info-item">
+      <b>Comments</b>${comments}
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>${downloads}
+    </p>
+  </div>
+</div></a>`;
+      }
+    )
+    .join('');
+  galleryEl.insertAdjacentHTML('beforeend', markup);
+};
+
+const getImages = async (query, pageToFetch) => {
+  try {
+    
+    const data = await fetchImages(query, pageToFetch);
+    
+    if (!data.hits.length) {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      return;
+      }
+      
+    renderImages(data);
+
+    lightbox.refresh();
+
+    if (pageToFetch === 1) {
+      Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+    }
+
+    if (data.totalHits >= pageToFetch * pageLimit) {
+      buttonLoadMore.classList.replace('is-hidden', 'load-more');
+    }
+
+    if (data.totalHits <= pageToFetch * pageLimit) {
+      Notiflix.Notify.info(
+        "We're sorry, but you've reached the end of search results"
+      );
+    }
+  } catch (error) {
+    console.log(error);
+    Notiflix.Notify.failure('Oops! Something went wrong!');
+  }
+};
